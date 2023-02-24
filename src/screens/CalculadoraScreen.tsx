81c4c5a9232b914ca369c572837c47e7cb391bf9
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-02-23 23:14:13 -0600

    Btn operaciones aritmeticas

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Text, View } from 'react-native';
 import { BotonCalc } from '../components/BotonCalc';
 import { styles } from '../theme/appTheme';
 
-
+//Para usar en el useRef
+enum Operadores{
+  sumar, restar,multiplicar,dividir
+}
 export const CalculadoraScreen = () => {
 
   // PAra mantener el estado
   const [numeroAnterior, setNumeroAnterior] = useState('0');
   const [numero, setNumero] = useState('0');
 
+
+  const ultimaOperacion = useRef<Operadores>()
+
+
   const limpiar = ()=>{
     setNumero('0');
+    setNumeroAnterior('0');
   }
   const armarNumero = (numeroTexto: string)=>{
     //No aceptar doble punto
@@ -53,6 +61,14 @@ export const CalculadoraScreen = () => {
       setNumero('0');
     }
   }
+ const cambiarNumPorAnterior =() => {
+    if(numero.endsWith('.')){
+      setNumeroAnterior(numero.slice(0,-1));
+    }else{
+      setNumeroAnterior(numero);
+    }
+    setNumero('0');
+ }
 
   const positivoNegativo = ()=>{
     if(numero.includes('-')){
@@ -61,9 +77,32 @@ export const CalculadoraScreen = () => {
       setNumero('-' + numero);
     }
   }
+
+  const btnDividir = () =>{
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.dividir;
+  }
+  const btnSumar = () =>{
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.sumar;
+  }
+  const btnRestar = () =>{
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.restar;
+  }
+  const btnMultiplicar = () =>{
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.multiplicar;
+  }
+ 
   return (
     <View style ={styles.calculadoraContainer}>
+      {
+        (numeroAnterior !=='0')&& (
         <Text style = { styles.resultadoPequeno}>{numeroAnterior}</Text>
+        )
+      }
+        
         <Text 
           style = { styles.resultado}
           numberOfLines={1}
@@ -77,28 +116,28 @@ export const CalculadoraScreen = () => {
             <BotonCalc texto="C" color ="#9B9B9B" accion = {limpiar}/>
             <BotonCalc texto="+/-" color ="#9B9B9B"  accion = {positivoNegativo}/>
             <BotonCalc texto="del" color ="#9B9B9B"  accion = {btnDelete}/>
-            <BotonCalc texto="/" color ="#FF9427"  accion = {limpiar}/>            
+            <BotonCalc texto="/" color ="#FF9427"  accion = {btnDividir}/>            
         </View>
         {/* Fila de botones  */}
         <View style = {styles.fila}>
             <BotonCalc texto="7" accion={armarNumero}/>
             <BotonCalc texto="8" accion={armarNumero}/>
             <BotonCalc texto="9" accion={armarNumero}/>
-            <BotonCalc texto="X" color ="#FF9427"  accion = {limpiar}/>            
+            <BotonCalc texto="X" color ="#FF9427"  accion = {btnMultiplicar}/>            
         </View>
         {/* Fila de botones  */}
         <View style = {styles.fila}>
             <BotonCalc texto="4" accion={armarNumero}/>
             <BotonCalc texto="5" accion={armarNumero}/>
             <BotonCalc texto="6" accion={armarNumero}/>
-            <BotonCalc texto="-" color ="#FF9427"  accion = {limpiar} />            
+            <BotonCalc texto="-" color ="#FF9427"  accion = {btnRestar} />            
         </View>
         {/* Fila de botones  */}
         <View style = {styles.fila}>
             <BotonCalc texto="1" accion={armarNumero}/>
             <BotonCalc texto="2" accion={armarNumero}/>
-            <BotonCalc texto="3" accion={armarNumero}/>
-            <BotonCalc texto="+" color ="#FF9427"  accion = {limpiar}/>            
+            <BotonCalc texto="3" accion={armarNumero}/> 
+            <BotonCalc texto="+" color ="#FF9427"  accion = {btnSumar}/>            
         </View>
         {/* Fila de botones  */}
         <View style = {styles.fila}>
