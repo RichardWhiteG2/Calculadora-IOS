bfbd63327ee1b6ad66fc05b4ebf4e2a3372b70b5
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-02-23 23:54:12 -0600

    RealizandoOperaciones.

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -94,6 +94,27 @@ export const CalculadoraScreen = () => {
     cambiarNumPorAnterior();
     ultimaOperacion.current = Operadores.multiplicar;
   }
+
+  const calcular = () =>{
+    const num1 = Number( numero);
+    const num2 = Number( numeroAnterior);
+    switch (ultimaOperacion.current) {
+      case Operadores.sumar:
+        setNumero(`${num1+num2}`);
+        break;
+      case Operadores.restar:
+        setNumero(`${num2-num1}`);
+        break;
+      case Operadores.multiplicar:
+        setNumero(`${num1*num2}`);
+        break;
+      case Operadores.dividir:
+        // operador ternario
+        setNumero(num1 !== 0 ? `${num2/num1}` : 'Syntaxis ERROR');
+        break;
+    }
+    setNumeroAnterior('0');
+  }
  
   return (
     <View style ={styles.calculadoraContainer}>
@@ -144,7 +165,7 @@ export const CalculadoraScreen = () => {
             
             <BotonCalc texto="0" ancho accion={armarNumero} />
             <BotonCalc texto="." accion={armarNumero}/>
-            <BotonCalc texto="=" color ="#FF9427"  accion = {limpiar}/>            
+            <BotonCalc texto="=" color ="#FF9427"  accion = {calcular}/>            
         </View>
     </View>
   )
